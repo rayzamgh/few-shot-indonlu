fdf9a1fd9a984963f1bdce3ced73ebaca38c26c2
Rayza Mahendra <rayzaganteng@gmail.com>
2022-08-20 20:17:52 +0700

    xglm test

--- script.py
+++ script.py
@@ -46,12 +46,12 @@ from transformers import XGLMTokenizer, XGLMForCausalLM
 # tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-2.7B')
 # gpt_model = GPTNeoForCausalLM.from_pretrained('EleutherAI/gpt-neo-1.3B')
 # tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
-# gpt_model = XGLMForCausalLM.from_pretrained("facebook/xglm-2.9B")
-# tokenizer = XGLMTokenizer.from_pretrained("facebook/xglm-2.9B")
-gpt_model = XGLMForCausalLM.from_pretrained("facebook/xglm-4.5B")
-tokenizer = XGLMTokenizer.from_pretrained("facebook/xglm-4.5B")
+gpt_model = XGLMForCausalLM.from_pretrained("facebook/xglm-2.9B")
+tokenizer = XGLMTokenizer.from_pretrained("facebook/xglm-2.9B")
+# gpt_model = XGLMForCausalLM.from_pretrained("facebook/xglm-4.5B")
+# tokenizer = XGLMTokenizer.from_pretrained("facebook/xglm-4.5B")
 
-model_name = 'facebook/xglm-4.5B'
+model_name = 'facebook/xglm-2.9B'
 model_token_limit = 1900
 
 model = gpt_model
@@ -520,21 +520,22 @@ import numpy as np
 import torch.nn.functional as F
 
 def get_logprobs(model, tokenizer, prompt, past_key_values, k_value, cuda):
-    inputs = tokenizer(prompt, return_tensors="pt")
-    
-    if k_value != 0 :
+    with torch.inference_mode():
+      inputs = tokenizer(prompt, return_tensors="pt")
+      
+      if k_value != 0 :
         past_key_values = cpu_past_to_gpu(past_key_values)
 
-    if cuda:
+      if cuda:
         input_ids_cuda = inputs["input_ids"].cuda(cuda7)
-    else:
+      else:
         input_ids_cuda = inputs["input_ids"]
 
-    input_ids, output_ids = input_ids_cuda, input_ids_cuda[:, 1:]
-    outputs = model(input_ids_cuda, past_key_values=past_key_values, labels=input_ids)
-    logits = outputs.logits
-    logprobs = torch.gather(F.log_softmax(logits, dim=2), 2, output_ids.unsqueeze(2))
-    return logprobs
+      input_ids, output_ids = input_ids_cuda, input_ids_cuda[:, 1:]
+      outputs = model(input_ids_cuda, past_key_values=past_key_values, labels=input_ids)
+      logits = outputs.logits
+      logprobs = torch.gather(F.log_softmax(logits, dim=2), 2, output_ids.unsqueeze(2))
+      return logprobs
 
 def eval_example(model, tokenizer, prefix, targets, k_past_context, k_past_values, k_value, k_context_length, cuda=True):
     
@@ -559,6 +560,7 @@ def calculate_log_prob_global(model, tokenizer, prefix, targets,  k_past_context
 
     scores = []
     for idx_target, c in enumerate(targets):
+      with torch.inference_mode():
         
         score = 0
         input_ids = prefix[0] + c
@@ -603,48 +605,51 @@ import numpy as np
 def predict_past(model, encoded_input_ids, cuda):
 
     cpu_ret_past_key_values = []
+
+    with torch.inference_mode():
         
-    outputs = model(encoded_input_ids)
+        outputs = model(encoded_input_ids)
 
-    # past_key_values 
-    ret_past_key_values = outputs.past_key_values
-    next_token = torch.argmax(outputs.logits[..., -1, :])
-    next_context = next_token.unsqueeze(0).unsqueeze(0)
+        # past_key_values 
+        ret_past_key_values = outputs.past_key_values
+        next_token = torch.argmax(outputs.logits[..., -1, :])
+        next_context = next_token.unsqueeze(0).unsqueeze(0)
 
-    if cuda:
-        torch.cuda.empty_cache()
+        if cuda:
+          torch.cuda.empty_cache()
 
-    del outputs
-    del next_token
+        del outputs
+        del next_token
 
-    # (32, 2)
-    # np.shape(ret_past_key_values)
+        # (32, 2)
+        # np.shape(ret_past_key_values)
 
-    for keyval_layer in ret_past_key_values:
-        cpu_ret_past_key_values.append((keyval_layer[0].cpu(), keyval_layer[1].cpu()))
+        for keyval_layer in ret_past_key_values:
+          cpu_ret_past_key_values.append((keyval_layer[0].cpu(), keyval_layer[1].cpu()))
 
-    cpu_ret_past_key_values = tuple(cpu_ret_past_key_values)
+        cpu_ret_past_key_values = tuple(cpu_ret_past_key_values)
 
-    del ret_past_key_values
+        del ret_past_key_values
 
     return next_context, cpu_ret_past_key_values
 
 def get_past_key_values(k, model, input_query, cuda, k_past_context, k_past_values):
+    with torch.inference_mode():
 
-    encoded_input_ids = tokenizer([input_query], return_tensors="pt")["input_ids"]
+        encoded_input_ids = tokenizer([input_query], return_tensors="pt")["input_ids"]
 
-    if cuda:  
-        encoded_input_ids = encoded_input_ids.cuda(cuda7)
+        if cuda:  
+            encoded_input_ids = encoded_input_ids.cuda(cuda7)
 
-    next_context, cpu_ret_past_key_values = predict_past(model, encoded_input_ids, cuda)
+        next_context, cpu_ret_past_key_values = predict_past(model, encoded_input_ids, cuda)
 
-    del encoded_input_ids
+        del encoded_input_ids
 
-    k_past_context[k] = next_context
-    k_past_values[k]  = cpu_ret_past_key_values
+        k_past_context[k] = next_context
+        k_past_values[k]  = cpu_ret_past_key_values
 
-    if cuda:
-        torch.cuda.empty_cache()
+        if cuda:
+          torch.cuda.empty_cache()
 
     return
 
@@ -702,6 +707,7 @@ def evaluate_next_word_k_way(tokenizer, k_value_hard_coded, labels, predict_clas
           
           if k != 0:
             for x in range(1):
+              with torch.inference_mode():
                 get_past_key_values(k, model, input_query, cuda, k_past_context, k_past_values)
 
         if cuda:
@@ -961,7 +967,7 @@ indoNLU_labels = {}
 indoNLU_data_classes = {}
 
 configs = {
-    "k_value_hard_coded" : [0, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80], 
+    "k_value_hard_coded" : [0, 1, 5, 10, 15, 20, 25, 30, 35, 40], 
     # "predict_class" :  normalize,
     "predict_class" :  calculate_log_prob_global, 
     "n_test_case" : 10,
